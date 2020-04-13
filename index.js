dbe289117d7118a14aa6eab0aab4b0b0c6c76bad
Sean O'Connor <soconnor@yahoo.com>
2020-04-12 21:01:00 -0500

    Small change

--- index.js
+++ index.js
@@ -45,8 +45,7 @@ const strategy = new Auth0Strategy(
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:
-      process.env.AUTH0_CALLBACK_URL
+    callbackURL: process.env.AUTH0_CALLBACK_URL
   },
   function(accessToken, refreshToken, extraParams, profile, done) {
     /**
